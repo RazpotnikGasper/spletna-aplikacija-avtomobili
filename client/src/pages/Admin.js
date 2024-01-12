561405b945f5d8680fd6e8ce836eff357c6f9943
RazpotnikGasper <35313026+RazpotnikGasper@users.noreply.github.com>
2024-01-12 17:52:22 +0100

    creating a navigate button to admin page

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CarManagement from '../container/CarManagement';
 import '../App.css';
 
 function Admin() {
+  // Use the useNavigate hook to get the navigate function
+  const navigate = useNavigate();
+
+  // Function to handle navigation to Home.js
+  const goToHome = () => {
+    navigate('/admin'); // Use the path of Home.js here
+  };
 
   return (
     <main>
       <div className="container" style={{ textAlign: 'center' }}>
         <CarManagement />
+        
+        {/* Button to navigate to Home.js */}
+        <button onClick={goToHome}>Go to Admin</button>
       </div>
     </main>
   );
